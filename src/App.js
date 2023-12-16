e9f658a7af499224bad09dc5421df74bf45073a8
yacoubou-seidou <yacoseidou@gmail.com>
2023-12-16 13:22:48 +0100

    add example display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import { PureIncrement, PureDecrement } from './lib/index'
 function App() {
   return (
     <div className="App">
       <h1>Welcome to My App</h1>
       <p>Happy Coding</p>
+      <PureIncrement start={0} end={10} duration={1} className="purecounter" />
+      <PureDecrement start={10} end={0} duration={1} className="purecounter" />
     </div>
   );
 }
